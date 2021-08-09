e78a655d836b45a3e53c6cc2a41f0badeb5eb938
lianrenjujishou <lianrenjujishou@aliyun.com>
2021-08-10 07:30:27 +0800

    new file server.go within new dir test

---
+++ test/server.go
@@ -0,0 +1,26 @@
+package main
+ import (
+   "log"
+   "net/http"
+   "io"
+ )
+
+
+//   req.ParseForm()
+//   log.Print(req.Form.Get("p1"), " ",req.Form.Get("p2"), " ",req.Form.Get("c"), " ", req.Form.Get("p3"), " ",req.Form.Get("p4"))
+//   io.WriteString(res, "<person><name>ZhangSan</name><email>ZhangSan@test.com</email></person> <person><name>Adam</name><email>adam@test.com</email></person> <person><name>Eve</name><email>eve@test.com</email></person>")
+func main() {
+  http.HandleFunc("/red", func(res http.ResponseWriter, req *http.Request){
+    io.WriteString(res, "RED")
+  })
+  http.HandleFunc("/a", func(res http.ResponseWriter, req *http.Request){
+    io.WriteString(res, "A")
+  })
+//  http.HandleFunc("/b", func(res http.ResponseWriter, req *http.Request){
+//  http.Redirect(res, req, "/red", 301)
+//  })
+  h := http.RedirectHandler("/red", 500)
+  http.Handle("/b", h)
+   log.Fatal(http.ListenAndServe(":8090", nil))
+ }
+ 
